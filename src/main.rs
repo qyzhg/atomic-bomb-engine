aee3fdc9f7b3bda160e498b7750956a6ab29c6ec
qyzhg <qyzhg@qyzhg.com>
2024-02-26 15:57:19 +0800

    feat: 增加吞吐量

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::sync::{Arc, Mutex};
 use tokio::time::interval;
 use indicatif::ProgressBar;
 use clap::Parser;
-use bytes;
 
 struct TestResult {
     total_duration: Duration,
@@ -19,6 +18,8 @@ struct TestResult {
     max_response_time: u64,
     min_response_time: u64,
     err_count: i32,
+    total_data_kb: f64,
+    throughput_per_second_kb: f64,
 }
 
 async fn run(url: &str, test_duration_secs: u64, concurrent_requests: i32) -> Result<TestResult, Error> {
@@ -31,37 +32,51 @@ async fn run(url: &str, test_duration_secs: u64, concurrent_requests: i32) -> Re
     let min_response_time = Arc::new(Mutex::new(u64::MAX));
     let err_count = Arc::new(Mutex::new(0));
     let mut handles = Vec::new();
+    let total_response_size = Arc::new(Mutex::new(0u64));
 
     for _ in 0..concurrent_requests {
         let client = reqwest::Client::new();
         let url = url.to_string();
         let histogram_clone = histogram.clone();
         let successful_requests_clone = successful_requests.clone();
-        let total_requests_clone = total_requests.clone();
         let test_end = test_end;
         let max_response_time_clone = max_response_time.clone();
         let min_response_time_clone = min_response_time.clone();
         let err_count_clone = err_count.clone();
+        let total_response_size_clone = total_response_size.clone();
+        let total_requests_clone = total_requests.clone();
 
         let handle = tokio::spawn(async move {
             while Instant::now() < test_end {
+                // 总请求数+1
                 *total_requests_clone.lock().unwrap() += 1;
                 let start = Instant::now();
                 match client.get(&url).send().await {
+                    // 请求成功
                     Ok(response) if response.status().is_success() => {
                         let duration = start.elapsed().as_millis() as u64;
+                        // 最大响应时间
                         let mut max_rt = max_response_time_clone.lock().unwrap();
-                        let mut min_rt = min_response_time_clone.lock().unwrap();
                         *max_rt = (*max_rt).max(duration);
+                        // 最小响应时间
+                        let mut min_rt = min_response_time_clone.lock().unwrap();
                         *min_rt = (*min_rt).min(duration);
+                        // 成功数量
+                        *successful_requests_clone.lock().unwrap() += 1;
+                        // 把响应时间加入统计
                         match histogram_clone.lock().unwrap().increment(duration) {
                             Ok(_) => {},
                             Err(err) => eprintln!("错误:{}", err),
                         }
-                        *successful_requests_clone.lock().unwrap() += 1;
+                        // 计算响应体大小并更新总大小
+                        if let Some(content_length) = response.content_length() {
+                            let mut total_size = total_response_size_clone.lock().unwrap();
+                            *total_size += content_length;
+                        }
                     },
-                    _ => {
+                    err => {
                         *err_count_clone.lock().unwrap() += 1;
+                        println!("err:{:?}", err)
                     }
                 }
             }
@@ -69,20 +84,23 @@ async fn run(url: &str, test_duration_secs: u64, concurrent_requests: i32) -> Re
 
         handles.push(handle);
     }
-    // 打印进度条
-    let pb = ProgressBar::new(100);
-    let progress_interval = Duration::from_millis(300); // 更新间隔设置为300毫秒
-    let mut interval = interval(progress_interval);
 
-    tokio::spawn(async move {
-        while Instant::now() < test_end {
-            interval.tick().await;
-            let elapsed = Instant::now().duration_since(test_start).as_secs_f64();
-            let progress = (elapsed / test_duration_secs as f64) * 100.0;
-            pb.set_position(progress as u64);
-        }
-        pb.finish_with_message("done");
-    }).await.unwrap();
+    {
+        // 打印进度条
+        let pb = ProgressBar::new(100);
+        let progress_interval = Duration::from_millis(300);
+        let mut interval = interval(progress_interval);
+
+        tokio::spawn(async move {
+            while Instant::now() < test_end {
+                interval.tick().await;
+                let elapsed = Instant::now().duration_since(test_start).as_secs_f64();
+                let progress = (elapsed / test_duration_secs as f64) * 100.0;
+                pb.set_position(progress as u64);
+            }
+            pb.finish_with_message("done");
+        }).await.unwrap();
+    }
 
     for handle in handles {
         handle.await.unwrap();
@@ -93,6 +111,9 @@ async fn run(url: &str, test_duration_secs: u64, concurrent_requests: i32) -> Re
     let successful_requests = *successful_requests.lock().unwrap() as f64;
     let success_rate = successful_requests / total_requests * 100.0;
     let histogram = histogram.lock().unwrap();
+    let total_response_size_kb = *total_response_size.lock().unwrap() as f64 / 1024.0;
+    let throughput_kb_s = total_response_size_kb / test_duration_secs as f64;
+
 
     let test_result = TestResult {
         total_duration,
@@ -105,6 +126,8 @@ async fn run(url: &str, test_duration_secs: u64, concurrent_requests: i32) -> Re
         max_response_time: *max_response_time.lock().unwrap(),
         min_response_time: *min_response_time.lock().unwrap(),
         err_count:*err_count.lock().unwrap(),
+        total_data_kb:total_response_size_kb,
+        throughput_per_second_kb: throughput_kb_s,
     };
 
     Ok(test_result)
@@ -141,6 +164,8 @@ async fn main() {
             println!("中位响应时间: {} ms", result.median_response_time);
             println!("95%响应时间: {} ms", result.response_time_95);
             println!("99%响应时间: {} ms", result.response_time_99);
+            println!("总吞吐量:{:.2}kb", result.total_data_kb);
+            println!("每秒吞吐量:{:.2}kb", result.throughput_per_second_kb);
         },
         Err(e) => println!("Error: {}", e),
     }
