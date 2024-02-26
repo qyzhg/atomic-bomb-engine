0d29547db4ff01ecab616ca22eb32a2b95bcf0c0
qyzhg <qyzhg@qyzhg.com>
2024-02-26 17:44:32 +0800

    feat: 格式化输出

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use reqwest;
 use tokio;
 use histogram::{Histogram};
@@ -8,7 +8,7 @@ use tokio::time::interval;
 use indicatif::ProgressBar;
 use clap::Parser;
 use anyhow::{Result, Context};
-use reqwest::{Error, Response, StatusCode};
+use prettytable::{Table, Row, Cell, format, row};
 
 
 struct TestResult {
@@ -199,28 +199,53 @@ struct Args {
     #[arg(long, default_value_t = 0)]
     timeout: u64,
 }
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
 
     match run(&args.url, args.duration_secs, args.concurrent_requests, args.timeout).await {
         Ok(result) => {
-            println!("测试完成");
-            println!("RPS:{:.3}", result.rps);
-            println!("总请求数：{:?}", result.total_requests);
-            println!("错误数量：{:?}", result.err_count);
-            println!("成功率: {:.2}%", result.success_rate);
-            println!("最大响应时间:{:.2}ms", result.max_response_time);
-            println!("最小响应时间:{:.2}ms", result.min_response_time);
-            println!("中位响应时间: {} ms", result.median_response_time);
-            println!("95%响应时间: {} ms", result.response_time_95);
-            println!("99%响应时间: {} ms", result.response_time_99);
-            println!("总吞吐量:{:.2}kb", result.total_data_kb);
-            for e in result.http_errors{
-                println!("{:03} | 错误:{:?},次数:{}", e.0.0,e.0.1, e.1)
+            // Create a new table for the results
+            let mut table = Table::new();
+            table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+            table.add_row(row!["指标", "值"]);
+            table.add_row(row!["RPS", format!("{:.3}", result.rps)]);
+            table.add_row(row!["总请求数", format!("{:?}", result.total_requests)]);
+            table.add_row(row!["错误数量", format!("{:?}", result.err_count)]);
+            table.add_row(row!["成功率", format!("{:.2}%", result.success_rate)]);
+            table.add_row(row!["最大响应时间", format!("{:.2}ms", result.max_response_time)]);
+            table.add_row(row!["最小响应时间", format!("{:.2}ms", result.min_response_time)]);
+            table.add_row(row!["中位响应时间", format!("{} ms", result.median_response_time)]);
+            table.add_row(row!["95%响应时间", format!("{} ms", result.response_time_95)]);
+            table.add_row(row!["99%响应时间", format!("{} ms", result.response_time_99)]);
+            table.add_row(row!["总吞吐量", format!("{:.2}kb", result.total_data_kb)]);
+
+            // Print the results table
+            table.printstd();
+
+            if !result.http_errors.is_empty() {
+                // Create a new table for the errors
+                let mut errors_table = Table::new();
+                errors_table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+
+                errors_table.add_row(row!["错误代码", "错误信息", "次数"]);
+                for e in result.http_errors {
+                    errors_table.add_row(Row::new(vec![
+                        Cell::new(format!("{:03}", e.0.0).as_str()),
+                        Cell::new(&format!("{:?}", e.0.1)).style_spec("R"),
+                        Cell::new(format!("{}", e.1).as_str()),
+                    ]));
+                }
+
+                // Print the errors table
+                println!("HTTP 错误:");
+                errors_table.printstd();
             }
 
         },
         Err(e) => println!("Error: {}", e),
     }
 }
+
