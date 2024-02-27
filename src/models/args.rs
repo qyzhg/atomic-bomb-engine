8417accc72adbb9d31567d62279731428a74421a
qyzhg <qyzhg@qyzhg.com>
2024-02-28 00:45:37 +0800

    feat: 更新帮助文档

--- src/models/args.rs
+++ src/models/args.rs
@@ -1,13 +1,17 @@
 use clap_derive::Parser;
 use clap::{ArgAction};
 
-
+/// 轻量级高性能压测引擎
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// 目标地址
     #[arg(short, long, required = true)]
     pub(crate) url: String,
+
+    /// 请求方法
+    #[arg(short, long, default_value = "GET")]
+    pub(crate) method: String,
 
     /// 持续时间（秒）
     #[arg(short, long, default_value_t = 1)]
@@ -21,23 +25,19 @@ pub struct Args {
     #[arg(long, default_value_t = 0)]
     pub(crate) timeout: u64,
 
-    /// 打印详情
+    /// 打印请求详情,关闭进度条
     #[arg(short, long, default_value_t = false)]
     pub(crate) verbose: bool,
-
-    /// 请求方法
-    #[arg(short, long, default_value = "GET")]
-    pub(crate) method: String,
 
     /// json
     #[arg(short, long)]
     pub(crate) json: Option<String>,
 
-    /// form表单
+    /// form表单(key1=val1&key2=val2...)
     #[arg(short, long)]
     pub(crate) form: Option<String>,
 
-    /// 设置HTTP头部
+    /// 设置HTTP头部，使用英文冒号分割(key:val)多个请求头可以使用多个-H或者--header参数
     #[clap(short = 'H', long = "header", action = ArgAction::Append)]
     pub headers: Option<Vec<String>>,
 
